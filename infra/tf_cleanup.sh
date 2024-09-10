98d5c63c02e8d092b3c2e88f0498cf761ccc4fc1
ahmadalsajid <ahmadalsajid@gmail.com>
2024-09-11 04:08:54 +0600

    minor update

--- infra/tf_cleanup.sh
+++ infra/tf_cleanup.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/bash
-
+start=$(date +%s)
 kubectl delete --all nodeclaim
 kubectl delete --all nodepool
 kubectl delete --all ec2nodeclass
 cluster_arn=$(kubectl config get-clusters | grep "arn:aws:eks:")
 kubectl config  delete-cluster "${cluster_arn}"
 terraform destroy --auto-approve
+end=$(date +%s)
+echo Execution time was "$((end - start))" seconds.
