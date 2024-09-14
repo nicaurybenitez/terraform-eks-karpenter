81cb6d20b6f63d2c70c81dfa7e5531bc2a37e6be
ahmadalsajid <ahmadalsajid@gmail.com>
2024-09-15 01:39:36 +0600

    minor update]

--- infra/tf_cleanup.sh
+++ infra/tf_cleanup.sh
@@ -2,8 +2,8 @@
 start=$(date +%s)
 kubectl delete -f ../EKS/echoserver_full.yml
 kubectl delete -f grafana.yml
-# spare 20 seconds for the above commands to remove the assets
-sleep 20
+# spare 60 seconds for the above commands to remove the assets
+sleep 60
 # https://github.com/prometheus-community/helm-charts/tree/main/charts/kube-prometheus-stack
 kubectl delete crd alertmanagerconfigs.monitoring.coreos.com
 kubectl delete crd alertmanagers.monitoring.coreos.com
